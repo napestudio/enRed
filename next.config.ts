d85ef8d8c6777deeea312b906d53ca5ce47f8f7f
cuerojr <cannibal5033@gmail.com>
2026-01-13 19:16:55 -0300

    asd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['en-red.vercel.app', 'cdn.sanity.io'],
+  },
 };
 
 export default nextConfig;
